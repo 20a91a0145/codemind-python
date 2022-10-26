cb4cbbfb1d52b604724d888d6531cff0810c467c
20a91a0145 <109515984+20a91a0145@users.noreply.github.com>
2022-10-26 15:00:13 +0530

    def pfs(n):     s=0     for i in range(1,n):         if n%i==0:
                s+=i
        return s
    a=int(input())
    b=int(input())
    if pfs(a)==b and pfs(b)==a:
        print('Amicable')
    else:
        print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,18 +1,13 @@
-n=int(input())
-m=int(input())
-# proper factor sum of n
-sum1=0
-for i in range(1,n):
-    if n%i==0:
-        sum1+=i
-# proper factor sum of m
-sum2=0
-for i in range(1,m):
-    if m%i==0:
-        sum2+=i
-        
-# condition
-if sum1==m and sum2==n:
+def pfs(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            
+            s+=i
+    return s
+a=int(input())
+b=int(input())
+if pfs(a)==b and pfs(b)==a:
     print('Amicable')
 else:
     print('Not Amicable')
